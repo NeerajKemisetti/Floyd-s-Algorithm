f27fcb1fa04728a5f5da0d2e5319de26762fd31f
Neeraj Kemisetti <70489410+NeerajKemisetti@users.noreply.github.com>
2020-11-25 14:45:07 +0530

    Add files via upload

---
+++ Floyds_Algo_function.cpp
@@ -0,0 +1,20 @@
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
